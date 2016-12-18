9bb64e83a4736297803f8a91a34269b55df5456e
Guillaume Serneels <guillaume.serneels@heig-vd.ch>
2016-12-18 20:14:40 +0100

    Add PicoContainer to share data between steps

---
+++ ExecutableSpecification/src/test/java/ch/heigvd/amt/gamification/spec/steps/SharedData.java
@@ -0,0 +1,16 @@
+package ch.heigvd.amt.gamification.spec.steps;
+
+/**
+ * Created by guillaume on 18.12.16.
+ */
+public class SharedData {
+    private int statusCode;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+}
---
+++ ExecutableSpecification/src/test/java/ch/heigvd/amt/gamification/spec/steps/SharedSteps.java
@@ -0,0 +1,32 @@
+package ch.heigvd.amt.gamification.spec.steps;
+
+import ch.heigvd.gamification.ApiException;
+import ch.heigvd.gamification.ApiResponse;
+import ch.heigvd.gamification.api.DefaultApi;
+import ch.heigvd.gamification.api.dto.Application;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by guillaume on 18.12.16.
+ */
+public class SharedSteps {
+
+    private SharedData world;
+
+    public SharedSteps(SharedData world){
+        this.world = world;
+    }
+    @Then("^I receive a (\\d+) status code$")
+    public void i_receive_a_status_code(int arg1) throws Throwable {
+        assertEquals(arg1, world.getStatusCode());
+
+    }
+}
+
+
+
