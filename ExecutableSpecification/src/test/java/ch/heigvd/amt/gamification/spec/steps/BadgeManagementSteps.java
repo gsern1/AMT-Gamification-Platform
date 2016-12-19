3606b54767c0cbed507fd8d2b51eb324121fa844
Ornidon <thib.loiseau@gmail.com>
2016-12-19 11:39:12 +0100

    Adding some badge tests

--- ExecutableSpecification/src/test/java/ch/heigvd/amt/gamification/spec/steps/BadgeManagementSteps.java
+++ ExecutableSpecification/src/test/java/ch/heigvd/amt/gamification/spec/steps/BadgeManagementSteps.java
@@ -5,9 +5,20 @@
  */
 package ch.heigvd.amt.gamification.spec.steps;
 
+import ch.heigvd.gamification.ApiException;
+import ch.heigvd.gamification.ApiResponse;
+import ch.heigvd.gamification.api.DefaultApi;
+import ch.heigvd.gamification.api.dto.Application;
+import ch.heigvd.gamification.api.dto.Badge;
+import ch.heigvd.gamification.api.dto.Credentials;
+import ch.heigvd.gamification.api.dto.Token;
+
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -15,21 +26,65 @@ import cucumber.api.java.en.When;
  */
 public class BadgeManagementSteps {
 
+    private final DefaultApi api = new DefaultApi();
+
+    private Token token;
+    private Badge badge;
+    private ApiResponse response;
+    private SharedData world;
+    private int badgeNbr;
+
+    public BadgeManagementSteps(SharedData world){
+        this.world = world;
+    }
+
+
+    @Given("^a token for a new gamified application $")
+    public void aTokenForANewGamifiedApplicationA(int arg0) throws Throwable {
+
+        Application application = new Application();
+
+        String randomAppName = "random-app-1-" + System.currentTimeMillis();
+        application.setName(randomAppName);
+
+        String password = "56789";
+        application.setPassword(password);
+
+        Credentials credentials = new Credentials();
+        credentials.setName(randomAppName);
+        credentials.setPassword(password);
+
+        api.addApplicationWithHttpInfo(application);
+
+        ApiResponse<Token> response = api.loginApplicationWithHttpInfo(credentials);
+        token = response.getData();
+
+        badgeNbr = api.findBadges(token.getToken()).size();
+
+    }
+
+
     @Given("^I have an badge payload$")
     public void i_have_an_badge_payload() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+        badge = new Badge();
+        badge.setName("badge-" + System.currentTimeMillis());
+
     }
 
     @When("^I POST it to the /badges endpoint$")
     public void i_POST_it_to_the_badges_endpoint() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        response = api.addBadgeWithHttpInfo(badge,token.getToken());
+        world.setStatusCode(response.getStatusCode());
+
     }
 
-    @Given("^a token for a new gamified application A(\\d+)$")
-    public void aTokenForANewGamifiedApplicationA(int arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+
+
+    @And("^I receive a reference about the created badge$")
+    public void iReceiveAReferenceAboutTheCreatedBadge() throws Throwable {
+        String location = response.getHeaders().get("location").toString();
+        assertEquals(location, "/api/badges/" + badgeNbr);
         throw new PendingException();
     }
 }
