c7fd5807168e2bd65eca96c4dd1f65009cc0988d
servietsky777 <antoine.drabble@gmail.com>
2017-01-24 01:58:56 +0100

    Implement cookies on frontend

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-	$('#jqueryVersion').text(jQuery.fn.jquery);
+	$( "#username" ).val($.cookie("username"));
+	$( "#username" ).on("input", function() {
+		console.log("wtf");
+	  $.cookie("username", $("username").val());console.log($.cookie());
+	});
+	$( "#cat1" ).click(function() {
+		$.post( "127.0.0.1:8090", {type: "cat1Click", username: "user1"}, function(data) {
+			$( ".result" ).html( data );
+		});
+	});
 });
