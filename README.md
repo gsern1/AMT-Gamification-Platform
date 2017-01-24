28909686d42c8c07bccc4fd0924d825e1de79586
antoine drabble <antoine.drabble@gmail.com>
2017-01-24 12:51:01 +0100

    gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# AMT-Gamification-Platform
-Repository of the Gamification Platform in the AMT 2016-20171 course at HEIG-VD
--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
+	var cat1cpt = 0;
+	var cat2cpt = 0;
 	$( "#username" ).val($.cookie("username"));
 	$( "#username" ).on("input", function() {
 		console.log("wtf");
 	  $.cookie("username", $("username").val());console.log($.cookie());
 	});
 	$( "#cat1" ).click(function() {
+		cat1cpt++;
+		$("#cat1cpt").text(cat1cpt);
 		$.post( "127.0.0.1:8090/api/events", {type: "cat1Click", username: $.cookie("username")}, function(data) {
 			console.log("Posted a new cat1Click event " + data);
 		});
 	});
 	$( "#cat2" ).click(function() {
+		cat2cpt++;
+		$("#cat2cpt").text(cat2cpt);
 		$.post( "127.0.0.1:8090/api/events", {type: "cat2Click", username: $.cookie("username")}, function(data) {
 			console.log("Posted a new cat2Click event " + data);
 		});
