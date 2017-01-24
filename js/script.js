0cd499d1ec46f617d43c0f8456fdb83b01f7bc0b
antoine drabble <antoine.drabble@gmail.com>
2017-01-24 13:37:06 +0100

    Update design and implement localstorage + refresh button

--- js/script.js
+++ js/script.js
@@ -1,32 +1,43 @@
 $(document).ready(function() {
+	var updateTables = function(){
+		$.get( "127.0.0.1:8090/api/leaderboard", function(data) {
+			console.log("Received the leaderboard " + data);
+		});
+		$.get( "127.0.0.1:8090/api/user/" + localStorage.getItem("username") + "/badges", function(data) {
+			console.log("Received the user's badges " + data);
+		});
+		$.get( "127.0.0.1:8090/api/user/" + localStorage.getItem("username") + "/pointScales", function(data) {
+			console.log("Received the user's pointScales " + data);
+		});
+	}
+	
+	
 	var cat1cpt = 0;
 	var cat2cpt = 0;
-	$( "#username" ).val($.cookie("username"));
+	$( "#username" ).val(localStorage.getItem("username"));
+	updateTables();
 	$( "#username" ).on("input", function() {
-		console.log("wtf");
-	  $.cookie("username", $("username").val());console.log($.cookie());
+		if($("#username").val() != ""){
+			localStorage.setItem("username", $("#username").val());
+			updateTables();
+		}
+	});
+	$("#refresh").click(function(){
+		updateTables();
+		return false;
 	});
 	$( "#cat1" ).click(function() {
 		cat1cpt++;
 		$("#cat1cpt").text(cat1cpt);
-		$.post( "127.0.0.1:8090/api/events", {type: "cat1Click", username: $.cookie("username")}, function(data) {
+		$.post( "127.0.0.1:8090/api/events", {type: "cat1Click", username: localStorage.getItem("username")}, function(data) {
 			console.log("Posted a new cat1Click event " + data);
 		});
 	});
 	$( "#cat2" ).click(function() {
 		cat2cpt++;
 		$("#cat2cpt").text(cat2cpt);
-		$.post( "127.0.0.1:8090/api/events", {type: "cat2Click", username: $.cookie("username")}, function(data) {
+		$.post( "127.0.0.1:8090/api/events", {type: "cat2Click", username: localStorage.getItem("username")}, function(data) {
 			console.log("Posted a new cat2Click event " + data);
 		});
-	});
-	$.get( "127.0.0.1:8090/api/leaderboard", function(data) {
-		console.log("Received the leaderboard " + data);
-	});
-	$.get( "127.0.0.1:8090/api/user/" + $.cookie("username") + "/badges", function(data) {
-		console.log("Received the user's badges " + data);
-	});
-	$.get( "127.0.0.1:8090/api/user/" + $.cookie("username") + "/pointScales", function(data) {
-		console.log("Received the user's pointScales " + data);
 	});
 });
