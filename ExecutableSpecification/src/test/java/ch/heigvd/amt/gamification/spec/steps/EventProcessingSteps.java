295cc7b837a251f3c012356fdc9523f2e21b3ba2
Ornidon <thib.loiseau@gmail.com>
2017-01-24 23:55:14 +0100

    adding 2 badges test (concurency and normal)

--- ExecutableSpecification/src/test/java/ch/heigvd/amt/gamification/spec/steps/EventProcessingSteps.java
+++ ExecutableSpecification/src/test/java/ch/heigvd/amt/gamification/spec/steps/EventProcessingSteps.java
@@ -5,10 +5,17 @@
  */
 package ch.heigvd.amt.gamification.spec.steps;
 
+import ch.heigvd.gamification.ApiException;
+import ch.heigvd.gamification.ApiResponse;
+import ch.heigvd.gamification.api.DefaultApi;
+import ch.heigvd.gamification.api.dto.Event;
+import ch.heigvd.gamification.api.dto.User;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -16,22 +23,62 @@ import cucumber.api.java.en.When;
  */
 public class EventProcessingSteps {
 
-    @Given("^I have the number of badges obtained$")
-    public void iHaveTheNumberOfBadgesObtained() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+
+    private final DefaultApi api = new DefaultApi();
+
+    private SharedData world;
+
+    public EventProcessingSteps(SharedData world){
+        this.world = world;
+    }
+
+    private User[] users = null;
+
+    private ApiResponse[] responses;
+
+    @Given("^I have a list of (\\d+) new user$")
+    public void iHaveAListOfNewUser(int number) throws Throwable {
+        users = new User[number];
+        for(int i = 0; i < number ;++i){
+            users[i] = new User();
+            users[i].setUsername("User-" + System.currentTimeMillis());
+        }
 
     }
 
-
-    @Given("^(\\d+) user POST an BadgeTyped event$")
-    public void userPOSTAnBadgeTypedEvent(int arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-
+    @When("^(\\d+) user POST an BadgeTyped event$")
+    public void userPOSTAnBadgeTypedEvent(int number) throws Throwable {
+        for(int i = 0; i < number ;++i){
+            Event userEvent = new Event();
+            userEvent.setUsername(users[i].getUsername());
+            userEvent.setType(SharedData.BADGES_RULE_NAME1);
+            responses = new ApiResponse[number];
+            responses[i] = api.addEventWithHttpInfo(userEvent, world.getToken().getToken());
+        }
     }
 
-    @Then("^There should be (\\d+) more badges$")
-    public void thereShouldBeMoreBadges(int arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
+    @Then("^Each user should have a badge$")
+    public void eachUserShouldHaveABadge() throws Throwable {
+        for(ApiResponse r : responses ){
+            assertEquals(r.getStatusCode(),200);
+        }
+    }
 
+    @When("^(\\d+) user POST an BadgeTyped event simultaneously$")
+    public void userPOSTAnBadgeTypedEventSimultaneously(int number) throws Throwable {
+        for(int i = 0; i < number ;++i){
+            final int ii = i;
+            new Thread(() -> {
+                Event userEvent = new Event();
+                userEvent.setUsername(users[ii].getUsername());
+                userEvent.setType(SharedData.BADGES_RULE_NAME1);
+                responses = new ApiResponse[number];
+                try {
+                    responses[ii] = api.addEventWithHttpInfo(userEvent, world.getToken().getToken());
+                } catch (ApiException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
     }
 }
--- ExecutableSpecification/src/test/resources/scenarios/event_processing.feature
+++ ExecutableSpecification/src/test/resources/scenarios/event_processing.feature
@@ -5,14 +5,18 @@ Background:
   And I have a badge payload
   And I POST it to the /badges endpoint
   And I have a pointScale payload
-  And I POST a pointscale for that application to the /pointScales endpoint with the recieved token
+  And I POST it to the /pointScales endpoint
   And a badgeRule payload concerning the previously posted badge and pointScale
   And I POST it to the /badgeRule endpoint
   And a pointScaleRule payload concerning the previously posted poinScale
   And I POST it to the /pointScaleRule endpoint
 
-Scenario: some userd POST an event and obtain a badge
-  Given I have the number of badges obtained
-  Given 15 user POST an BadgeTyped event
-  Then There should be 15 more badges
+Scenario: some users POST an event and obtain a badge
+  Given I have a list of 15 new user
+  When 15 user POST an BadgeTyped event
+  Then Each user should have a badge
 
+Scenario: some users POST an event and obtain a badge simultaneously
+  Given I have a list of 15 new user
+  When 15 user POST an BadgeTyped event simultaneously
+  Then Each user should have a badge
