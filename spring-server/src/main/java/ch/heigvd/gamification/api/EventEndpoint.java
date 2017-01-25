8fdbd0cc2d3b5cd79356836b102ff0aeba3fc80a
antoine drabble <antoine.drabble@gmail.com>
2017-01-25 04:07:52 +0100

    Update eventprocessor to return a boolean

--- spring-server/src/main/java/ch/heigvd/gamification/api/EventEndpoint.java
+++ spring-server/src/main/java/ch/heigvd/gamification/api/EventEndpoint.java
@@ -62,7 +62,9 @@ public class EventEndpoint implements EventsApi {
         boolean done = false;
         while (!done) {
             try {
-                eventProcessor.processEvent(user, event);
+                if(!eventProcessor.processEvent(user, event)){
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+                }
                 done = true;
             } catch (ObjectOptimisticLockingFailureException e) {
                 System.out.println("FAILED TO UPDATE (concurrency)... LET'S GO AGAIN");
